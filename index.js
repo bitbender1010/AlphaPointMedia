754738c7ed59d6745ae51a7e3ee282e915e9c016
Dammie <bitbender1010@gmail.com>
2023-01-11 21:11:27 +0100

    redesign APM

---
+++ index.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('.mobile-nav-items')
+
+
+let style = document.querySelector('.nav-img')
+
+style.addEventListener('click', () => {
+  //show when clicked
+  console.log(menu.style)
+})
